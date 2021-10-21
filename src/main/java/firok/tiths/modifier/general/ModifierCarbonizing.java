4f46ba15ab90b05749c8e362360dd9ba6f22e84e
TT432 <2437315224@qq.com>
2021-10-21 11:49:47 +0800

    0.4.14.0 ModifierCarbonizing

--- src/main/java/firok/tiths/modifier/general/ModifierCarbonizing.java
+++ src/main/java/firok/tiths/modifier/general/ModifierCarbonizing.java
@@ -1,16 +1,27 @@
 package firok.tiths.modifier.general;
 
+import firok.tiths.config.ConfigModifier;
 import firok.tiths.util.Actions;
 import firok.tiths.util.DevUse;
 import firok.tiths.util.Predicates;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.tools.context.ToolHarvestContext;
 import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
@@ -24,37 +35,39 @@ public class ModifierCarbonizing extends Modifier
 		super(0x2b2b2b);
 	}
 
-	// fixme 匠魂3的这个方法不直接满足原本特性需要的参数
+	@Nullable
 	@Override
-	public void afterBlockBreak(IModifierToolStack tool, int level, ToolHarvestContext context)
-	{
-		super.afterBlockBreak(tool, level, context);
+	public Boolean removeBlock(IModifierToolStack tool, int level, ToolHarvestContext context) {
+		ServerWorld world = context.getWorld();
+		Random rand = world.rand;
+		ServerPlayerEntity player = context.getPlayer();
 
+		boolean playSound = false;
 
-//		super.blockHarvestDrops(tool, event);
-//		World world = context.getWorld();
-//		if (world.isRemote) return; // 只在服务端进行
-//		Random rand = world.rand;
-//		ServerPlayerEntity player = context.getPlayer();
-//
-//		int countCoals = 0;
-//
-//		List<ItemStack> drops = event.getDrops();
-//		Iterator<ItemStack> iter = drops.iterator();
-//		while (iter.hasNext())
-//		{
-//			ItemStack drop = iter.next();
-//			if (Predicates.isAnyStone(drop) && canTrigger(rand, Configs.Traits.rate_carbonizing_transform * drop.getCount()))
-//			{
-//				iter.remove();
-//				countCoals += drop.getCount();
-//			}
-//		}
-//		if (countCoals > 0)
-//		{
-//			Actions.CauseSpawnItem(player, new ItemStack(net.minecraft.init.Items.COAL, countCoals));
-//			player.world.playSound(null, player.posX, player.posY, player.posZ,
-//					SoundEvents.effectFriction, SoundCategory.MASTER, 1, 1);
-//		}
+		BlockState state = context.getState();
+		BlockPos pos = context.getPos();
+
+		List<ItemStack> drops = Block.getDrops(state, world, pos, world.getTileEntity(pos));
+
+		for (int i = 0; i < drops.size(); i++) {
+			ItemStack drop = drops.get(i);
+			if (ItemTags.STONE_BRICKS.contains(drop.getItem()) &&
+					Predicates.canTrigger(rand, ConfigModifier.rate_carbonizing_transform.get() * drop.getCount()))
+			{
+				drops.set(i, new ItemStack(Items.COAL, drop.getCount()));
+				playSound = true;
+			}
+		}
+
+		if (playSound)
+		{
+			// todo play sound
+			// player.world.playSound(null, player.posX, player.posY, player.posZ,
+			// 		SoundEvents.effectFriction, SoundCategory.MASTER, 1, 1);
+		}
+
+		drops.forEach(itemStack -> Actions.CauseSpawnItem(player, itemStack));
+
+		return false;
 	}
 }
