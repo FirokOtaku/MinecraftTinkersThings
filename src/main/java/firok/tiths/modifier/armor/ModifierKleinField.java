b81bc695756d1da8c0e542b9979d53ad25524bec
Firok <351768593@outlook.com>
2022-02-09 23:17:07 +0800

    0.4.37.1

---
+++ src/main/java/firok/tiths/modifier/armor/ModifierKleinField.java
@@ -0,0 +1,13 @@
+package firok.tiths.modifier.armor;
+
+import slimeknights.tconstruct.library.modifiers.Modifier;
+
+public class ModifierKleinField extends Modifier
+{
+	public ModifierKleinField()
+	{
+		super(0);
+	}
+
+
+}
